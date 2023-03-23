8f968d82f057ade67688ba0d20694be409e0ee27
Eduardo Ferreira <eduardoferreira@MacBook-Pro-de-Eduardo.local>
2023-03-22 22:10:34 -0300

    Estrutura Básica

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const card =  () => {
+
+  return (<h1>Card Component</h1>)
+}
---
+++ src/components/task/task.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const task = () =>{
+
+  return( <h1>Task Component</h1>)
+
+  }
+
