ea38f9339d2ac6805baaf6379fbccc24f8960f1a
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-04-14 12:17:08 -0300

    feature: Adicionando dias de atraso no componente de data e adicionando estilização em arquivo separado

---
+++ src/components/dashboard/datePeriod/datePeriodStyles.jsx
@@ -0,0 +1,39 @@
+const styles = {
+    root: {
+        display: 'flex', 
+        flexDirection: 'column'
+    },
+
+    datesDiv: {
+        display: 'flex', 
+        flexDirection: 'row', 
+        width: '250px', 
+        height: '45px', 
+        borderRadius: '10px', 
+        border: '1px solid #d9d9d9', 
+        alignItems: 'center', 
+        justifyContent: 'space-around'
+    },
+
+    dateIcons: {
+        color: '#bfbfbf'
+    },
+
+    daleyedDiv: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+
+    warningIcon: {
+        color: '#ff0000'
+    },
+
+    warningText: {
+        color: '#ff0000', marginLeft: '5px'
+    }
+}
+
+export default styles
