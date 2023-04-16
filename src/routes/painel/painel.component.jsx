08ca783f13b5dced0654b18466cd90e8f7e76738
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-04-16 16:56:20 -0300

    feat: Finalizando componente Sidebar

--- src/routes/painel/painel.component.jsx
+++ src/routes/painel/painel.component.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import DashboardItem from "../../components/dashboard/dashboardItem.component"
+import Sidebar from "../../components/sidebar/sidebar.component"
 
 const Painel = () =>{
 
   return(
-    <div id="root">
-      <h1> Painel </h1>
-      <DashboardItem projectProgress={75} startDate={new Date(2023, 2, 1)} endDate={new Date(2023, 6, 24)} managerName={"Eduardo Ferreira"} />
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+      <div style={{width: '20%'}}>
+        <Sidebar menuItem={0}/>
+      </div>
+      <div style={{display: 'flex', flexDirection: 'column', width: '80%'}}>
+        <h1> Painel </h1>
+        <DashboardItem projectProgress={75} startDate={new Date(2023, 2, 1)} endDate={new Date(2023, 6, 24)} managerName={"Eduardo Ferreira"} />
+      </div>
     </div>
   )
 }
