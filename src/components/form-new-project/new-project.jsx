99d7f152a9e6af50201749ae37722b18144391d5
Sistemas-Gerenciamento-Projetos <129122517+Sistemas-Gerenciamento-Projetos@users.noreply.github.com>
2023-05-17 20:33:56 -0300

    Adicionando formulario de novo projeto

---
+++ src/components/form-new-project/new-project.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./new-project.scss";
+
+const newproject = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        backgroundColor: "#ffffff",
+        marginTop: "20px",
+        marginBottom: "20px",
+        padding: "15px",
+      }}>
+      Formulario com os dados{" "}
+    </div>
+  );
+};
+
+export default newproject;
