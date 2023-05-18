379146c7d4b6f0107362bdbeb4066b04e874ca5f
Sistemas-Gerenciamento-Projetos <129122517+Sistemas-Gerenciamento-Projetos@users.noreply.github.com>
2023-05-17 22:03:41 -0300

    atualizando

--- src/components/form-new-project/new-project.jsx
+++ src/components/form-new-project/new-project.jsx
@@ -2,18 +2,7 @@ import React from "react";
 import "./new-project.scss";
 
 const newproject = () => {
-  return (
-    <div
-      style={{
-        height: "100%",
-        backgroundColor: "#ffffff",
-        marginTop: "20px",
-        marginBottom: "20px",
-        padding: "15px",
-      }}>
-      Formulario com os dados{" "}
-    </div>
-  );
+  return <div className="main">Formulario com os dados </div>;
 };
 
 export default newproject;
