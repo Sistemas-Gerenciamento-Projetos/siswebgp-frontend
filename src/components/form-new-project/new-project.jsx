656e295890c108230a808caf2624b27147e46ed0
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-05-24 15:23:25 -0300

    feature: adicionando request de criação de projeto e caso de sucesso

--- src/components/form-new-project/new-project.jsx
+++ src/components/form-new-project/new-project.jsx
@@ -1,6 +1,75 @@
 import React, { useState } from "react"
 import { Form, Button, InputGroup, Badge, } from "react-bootstrap"
 import "./new-project.scss"
+import axios from "axios"
+import { Navigate } from "react-router-dom"
+
+const jsonExample = {
+  "manager": "bbf36179-6550-452c-8322-dc12abd27a52",
+  "project_name": "Sistema de Gerência de Projetos",
+  "description": "Projeto para o software de sistema de gerenciamento de projetos",
+  "deadline_date": "2023-07-31",
+  "users": [
+    "2d9dcff9-6448-4b02-b603-786408c62035",
+    "bbf36179-6550-452c-8322-dc12abd27a52"
+  ]
+}
+
+function handleRegisterProject(managerId, title, description, beginDate, endDate) {
+  console.log(title)
+  console.log(description)
+  console.log(endDate)
+  const parsedTitle = title.trim()
+  const parsedDate = endDate == "" ? null : endDate.toISOString().split('T')[0]
+
+  const header = {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjg0OTUyMjA2LCJpYXQiOjE2ODQ5NTE5MDYsImp0aSI6IjBiYzY4ZjVkNmI0ZjRmZDFhZjMwMzA3NGViN2ZjYTA2IiwidXNlcl9pZCI6IjBiMzA1OGE2LTU0ZjYtNDQzOS04NWJmLTE2MWUxMmY2NDUxNiJ9.sPvqt4bRtFkSV2OXC9Qmt4Ayo3tSKkZ2X5anYVkP874`
+    }
+  }
+
+  axios.post(
+    "http://127.0.0.1:8000/api/projects/", // Colocar no arquivo de constants
+    {
+      manager: "0b3058a6-54f6-4439-85bf-161e12f64516", // trocar pelo que vem no context
+      project_name: parsedTitle,
+      description: description,
+      deadline_date: parsedDate,
+      users: ["0b3058a6-54f6-4439-85bf-161e12f64516"] // trocar pelo que vem no context
+    },
+    header
+  ).then((response) => {
+    if (response.status == 201) {
+      console.log(response)
+      redirectPage()
+    } else {
+      // Montar feedback caso ocorra algum erro na requisição
+    }
+  }).catch((error) => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    // retornar alert com mensagem generica de erro
+    alert(error)
+  })
+}
+
+const redirectPage = () => {
+  <Navigate replace to="/projetos/" />;
+};
 
 const Registration = ({ handleCadastro }) => {
   const [title, setTitle] = useState("")
@@ -19,8 +88,7 @@ const Registration = ({ handleCadastro }) => {
       return
     }
 
-    // Call API or perform form submission
-    //handleCadastro();
+    handleRegisterProject("", title, description, beginDate, endDate);
   }
 
   return (
@@ -29,7 +97,7 @@ const Registration = ({ handleCadastro }) => {
       {/* Tem um bug na validação de string com espaços em branco, o form nega o seguimento mas o feedback visual é de correto */}
       <Form.Group controlId="title">
         <Form.Label>Título:</Form.Label>
-        <InputGroup noValidate>
+        <InputGroup hasValidation>
           <Form.Control
             type="text"
             className="form-item"
@@ -84,6 +152,7 @@ const Registration = ({ handleCadastro }) => {
             type="date"
             isValid={Date.parse(beginDate) < Date.parse(endDate)}
             min={beginDate === "" ? new Date().toISOString().split('T')[0] : beginDate}
+            disabled={beginDate === ""}
             className="form-item"
             value={endDate}
             onChange={(e) => setEndDate(e.target.value)}
