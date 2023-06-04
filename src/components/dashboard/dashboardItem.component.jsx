dff0b5f18c2a0443b8c291f5abb4c8d64ebaf6e9
Eduardo Ferreira da Silva <eduardo.ferreira1983@gmail.com>
2023-06-04 13:16:54 -0300

    adicionando opcoes de projeto

--- src/components/dashboard/dashboardItem.component.jsx
+++ src/components/dashboard/dashboardItem.component.jsx
@@ -1,29 +1,40 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import ProgressBar from "./progressBar/progressBar";
 import DatePeriod from "./datePeriod/datePeriod";
 import ManagerPhoto from "./managerPhoto/managerPhoto";
 import { useProjectDetails } from "../../context/projectContext";
 import { parseDateWithoutTimezone } from "../../utils/dateParse";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 const DashboardItem = ({ project, onPress }) => {
-  const [projectDetails] = useProjectDetails()
-  const parsedStartDate = parseDateWithoutTimezone(project.creation_date)
-  const parsedEndDate = parseDateWithoutTimezone(project.deadline_date)
-  var progress = 0
+  const [projectDetails] = useProjectDetails();
+  const parsedStartDate = parseDateWithoutTimezone(project.creation_date);
+  const parsedEndDate = parseDateWithoutTimezone(project.deadline_date);
+  var progress = 0;
 
-  if (project.num_total_tasks != 0) {
-    progress = (project.num_completed_tasks / project.num_total_tasks) * 100
+  if (project.num_total_tasks !== 0) {
+    progress = (project.num_completed_tasks / project.num_total_tasks) * 100;
   }
-    
+
   return (
-    <tr 
-        style={{backgroundColor: project.id === projectDetails.projectId ? "#b0ecff" : ""}}
-        onClick={() => {onPress(project.id, project.project_name)}}
-    >
-      <td>{project.project_name}</td> 
-      <td><ProgressBar completed={progress} /></td>
-      <td><DatePeriod startDate={parsedStartDate} endDate={parsedEndDate} /></td>
-      <td><ManagerPhoto name={project.manager_name}/></td>
+    <tr
+      style={{
+        backgroundColor:
+          project.id === projectDetails.projectId ? "#bae7ff" : "",
+      }}
+      onClick={() => {
+        onPress(project.id, project.project_name);
+      }}>
+      <td>{project.project_name}</td>
+      <td>
+        <ProgressBar completed={progress} />
+      </td>
+      <td>
+        <DatePeriod startDate={parsedStartDate} endDate={parsedEndDate} />
+      </td>
+      <td>
+        <ManagerPhoto name={project.manager_name} />
+      </td>
     </tr>
   );
 };
