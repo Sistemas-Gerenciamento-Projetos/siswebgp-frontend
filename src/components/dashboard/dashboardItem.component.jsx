97c1eb04736d807b5c78a4daa6a5b373ddadd69e
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-06-20 22:33:19 -0300

    Adicionando deleção de tarefa implementada por Bruno

--- src/components/dashboard/dashboardItem.component.jsx
+++ src/components/dashboard/dashboardItem.component.jsx
@@ -4,19 +4,27 @@ import DatePeriod from "./datePeriod/datePeriod";
 import ManagerPhoto from "./managerPhoto/managerPhoto";
 import { useProjectDetails } from "../../context/projectContext";
 import { parseDateWithoutTimezone } from "../../utils/dateParse";
-import { hover } from "@testing-library/user-event/dist/hover";
 import Button from "react-bootstrap/Button";
 import TrashIcon from "../../Assets/trash.svg";
 import EditIcon from "../../Assets/edit.svg"; 
 import AddIcon from "../../Assets/person-add.svg";
-import { Border } from "devextreme-react/bar-gauge";
+import { useUserDetails } from "../../context/usercontext";
+import { deleteProject } from "../../services/projects/deleteProject";
+import { toast } from "react-toastify";
 
 
 const DashboardItem = ({ project, onPress, setIndex }) => {
   const [projectDetails] = useProjectDetails();
+  const [userDetails] = useUserDetails();
   const parsedStartDate = parseDateWithoutTimezone(project.creation_date);
   const parsedEndDate = parseDateWithoutTimezone(project.deadline_date);
   var progress = 0;
+
+  const handleDelete = (projectName) => {
+    if (window.confirm(`Você tem certeza que deseja deletar ${projectName}?`)) {
+      deleteProject(userDetails, projectDetails)
+    }
+  };
 
   if (project.num_total_tasks !== 0) {
     progress = (project.num_completed_tasks / project.num_total_tasks) * 100;
@@ -25,10 +33,7 @@ const DashboardItem = ({ project, onPress, setIndex }) => {
   return (
 
     <tr
-      style={{
-        backgroundColor:
-          project.id === projectDetails.projectId ? "#bae7ff" : "",
-      }}
+      style={{ backgroundColor: project.id === projectDetails.projectId ? "#bae7ff" : ""}}
       onClick={() => {
         onPress(project.id, project.project_name);
       }}>
@@ -43,24 +48,17 @@ const DashboardItem = ({ project, onPress, setIndex }) => {
         <ManagerPhoto name={project.manager_name} />
       </td>
       <td>
-        <Button 
-          variant="outline-light" 
-          style={{border:0}} 
-          onClick={() => setIndex(2)} 
-        >
-            <img src={AddIcon}/>
+        <Button variant="outline-light" style={{border:0}} onClick={() => setIndex(2)}>
+          <img src={AddIcon}/>
         </Button>
-        <Button variant="outline-light" style={{border:0}} onClick={() => setIndex(3)} >
-          <img
-            src={EditIcon}
-          />
+        
+        <Button variant="outline-light" style={{border:0}} onClick={() => setIndex(3)}>
+          <img src={EditIcon} />
         </Button> 
-        <Button  variant="outline-light"  style={{border:0}} >
-          <img
-            src={TrashIcon}
-          />
+        
+        <Button variant="outline-light"  style={{border:0}} onClick={() => {handleDelete(project.project_name)}}>
+          <img src={TrashIcon}/>
         </Button> 
-
       </td>
     </tr>
   );
