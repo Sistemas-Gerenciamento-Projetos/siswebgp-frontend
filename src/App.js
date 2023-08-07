6ade995bbc4fbd6949d75308c37da330e5c11ed2
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-08-07 19:04:03 -0300

    project: adicionando eslint + prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Authentication from "./pages/authentication/authentication";
-import { UserDetailsProvider } from "./context/usercontext";
-import Home from "./pages/home/home.component";
-import { Navigate } from "react-router-dom";
-import { ProjectDetailsProvider } from "./context/projectContext";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Authentication from './pages/authentication/authentication';
+import { UserDetailsProvider } from './context/usercontext';
+import Home from './pages/home/home.component';
+import { Navigate } from 'react-router-dom';
+import { ProjectDetailsProvider } from './context/projectContext';
 
 const App = () => {
   return (
