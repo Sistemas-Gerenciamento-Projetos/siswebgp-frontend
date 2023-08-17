a33d496a20acb5347ece5c42ee4fb9a861adbbd2
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-08-17 20:46:37 -0300

    feature: Refazendo request de login

--- src/components/form-login/login.jsx
+++ src/components/form-login/login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { Navigate } from 'react-router-dom';
 import { useUserDetails } from '../../context/usercontext';
-import { sigin } from '../../services/authorization/login';
+import { login } from '../../services/authorization/login';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -26,9 +26,11 @@ function Login() {
     ) {
       newErrors.email = 'Verifique seu email.';
     }
+
     if (!password || password === ' ' || password.length < 8) {
       newErrors.password = 'Verifique seus dados.';
     }
+
     return newErrors;
   };
 
@@ -41,21 +43,25 @@ function Login() {
       return;
     }
 
-    const logged = await sigin(email, password, userDetails, updateUserDetails);
-    if (logged) {
-      setErrors('');
-    } else {
-      toast.error('Credenciais inválidas', {
-        position: 'bottom-right',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: 'colored',
+    login(userDetails.accessToken, email, password)
+      .then((data) => {
+        setErrors('');
+        localStorage.setItem('userDetails', JSON.stringify(data));
+        updateUserDetails(data.access, data.refresh, data.user.id);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Credenciais inválidas', {
+          position: 'bottom-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
       });
-    }
   };
 
   useEffect(() => {
@@ -89,6 +95,10 @@ function Login() {
           className="form-item"
           isInvalid={!!errors.password}
         />
+
+        <Form.Control.Feedback type="invalid">
+          {errors.password}
+        </Form.Control.Feedback>
       </Form.Group>
 
       <div className="d-grid">
