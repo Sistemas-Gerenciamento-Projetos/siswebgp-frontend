42c1398d142f74af3f577bdb8581b10a03709968
AlbertoDoc <albertooliveirasantos@gmail.com>
2023-10-15 15:00:51 -0300

    feature: Adicionando loading na visualização de tarefas

--- src/components/epic-component/add-epic-tasks/view-task/viewTask.component.jsx
+++ src/components/epic-component/add-epic-tasks/view-task/viewTask.component.jsx
@@ -1,4 +1,4 @@
-import { Empty } from 'antd';
+import { Empty, Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { getEpicTasks } from '../../../../services/epics/getEpicTasks';
@@ -6,65 +6,77 @@ import { useUserDetails } from '../../../../context/usercontext';
 import { useProjectDetails } from '../../../../context/projectContext';
 import translateStatus from '../../../../utils/translateStatus';
 import { showErrorToast } from '../../../../utils/Toasts';
-import { EmptyDiv, TableHeader } from './viewTask.styles';
+import { EmptyDiv, SpinDiv, TableHeader } from './viewTask.styles';
 
 export default function ViewTask({ epicId }) {
   const [tasks, setTasks] = useState([]);
   const [userDetails] = useUserDetails();
   const [projectDetails] = useProjectDetails();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     getEpicTasks(userDetails.accessToken, projectDetails.projectId, epicId)
       .then((data) => {
         setTasks(data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
         showErrorToast('Erro ao buscar as tarefas do épico');
+        setLoading(false);
       });
   }, []);
 
   return (
     <>
-      {tasks.length !== 0 ? (
-        <>
-          <Table>
-            <thead>
-              <tr>
-                <th>
-                  <TableHeader>Id</TableHeader>
-                </th>
-                <th>
-                  <TableHeader>Titulo</TableHeader>
-                </th>
-                <th>
-                  <TableHeader>Descrição</TableHeader>
-                </th>
-                <th>
-                  <TableHeader>Status</TableHeader>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {tasks.map((task, index) => (
-                <tr key={task.id}>
-                  <td>
-                    <span>{task.number}</span>
-                  </td>
-                  <td>
-                    <span>{task.title}</span>
-                  </td>
-                  <td>{task.description}</td>
-                  <td>{translateStatus(task.status)}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </>
+      {loading ? (
+        <SpinDiv>
+          <Spin />
+        </SpinDiv>
       ) : (
-        <EmptyDiv>
-          <Empty description="Sem tarefas existentes" />
-        </EmptyDiv>
+        <>
+          {tasks.length !== 0 ? (
+            <>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>
+                      <TableHeader>Id</TableHeader>
+                    </th>
+                    <th>
+                      <TableHeader>Titulo</TableHeader>
+                    </th>
+                    <th>
+                      <TableHeader>Descrição</TableHeader>
+                    </th>
+                    <th>
+                      <TableHeader>Status</TableHeader>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tasks.map((task, index) => (
+                    <tr key={task.id}>
+                      <td>
+                        <span>{task.number}</span>
+                      </td>
+                      <td>
+                        <span>{task.title}</span>
+                      </td>
+                      <td>{task.description}</td>
+                      <td>{translateStatus(task.status)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </>
+          ) : (
+            <EmptyDiv>
+              <Empty description="Sem tarefas existentes" />
+            </EmptyDiv>
+          )}
+        </>
       )}
     </>
   );
--- src/components/epic-component/add-epic-tasks/view-task/viewTask.styles.jsx
+++ src/components/epic-component/add-epic-tasks/view-task/viewTask.styles.jsx
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
 export const EmptyDiv = styled.div`
-  height: 100%;
+  height: 300px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -10,4 +10,11 @@ export const EmptyDiv = styled.div`
 
 export const TableHeader = styled.p`
   font-weight: 600;
+`;
+
+export const SpinDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
 `;
